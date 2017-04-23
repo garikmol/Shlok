4e66295199ad4d3d436e69c3d81e06f8bd230fbb
Igor Molochnikov <garikmol@mail.ru>
2017-04-23 07:22:13 +0300

    секция 4

---
+++ app/javascript/swiper_4-1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Igor on 21.04.2017.
+ */
+
+var swiper4_1 = new Swiper ('.swiper-container4-1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 4,
+    // If we need pagination
+    pagination: '.swiper-pagination',
+    paginationType: 'progress'
+});
+
