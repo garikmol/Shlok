26860d4d1334116d383fbd7b64175dce6f7417e3
Igor Molochnikov <garikmol@mail.ru>
2017-04-25 23:12:13 +0300

    секция6-1

--- app/javascript/swiper_4-1.js
+++ app/javascript/swiper_4-1.js
@@ -13,4 +13,18 @@ var swiper4_1 = new Swiper ('.swiper-container4-1', {
     paginationType: 'progress'
 });
 
+var swiper6_1 = new Swiper ('.swiper-container6-1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 3,
+    // If we need pagination
+    pagination: '.swiper-pagination6-1',
+    paginationType: 'fraction',
+    watchSlidesProgress: true,
+    nextButton: '.swiper-button-next6-1'
+
+});
+
 
